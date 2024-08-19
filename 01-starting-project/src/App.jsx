4044acd4edce3169ea6c505d7f2ef23fc0633235
alicelee1231 <saemmilee1231@gmail.com>
2024-08-20 07:05:24 +0900

    component

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,14 +1,36 @@
+import reactImg from './assets/react-core-concepts.png'
+import componentsImg from './assets/components.png'
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
 function Header() {
+
+  const description = reactDescriptions[genRandomInt(2)]
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
+}
+
+function CoreConcept() {
+  return (
+    <li>
+      <img src="..." alt='...'/>
+      <h3>TITLE</h3>
+      <p>DESCRIPTION</p>
+    </li>
+  )
 }
 
 function App() {
@@ -16,10 +38,17 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-comcept">
+        <h2>Core Concept</h2>
+        <ul>
+          <CoreConcept title="Components" description="The core UI building block"
+          image={componentsImg}
+          />
+        </ul>
+        </section>
       </main>
     </div>
   );
 }
-
+// 
 export default App;
