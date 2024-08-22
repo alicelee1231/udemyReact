c8e755410309e8ae5040fa9513fa4d249d7490b6
alicelee1231 <saemmilee1231@gmail.com>
2024-08-23 07:01:09 +0900

    props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,15 +1,15 @@
-import reactImg from './assets/react-core-concepts.png'
-import componentsImg from './assets/components.png'
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1))
+  return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-
-  const description = reactDescriptions[genRandomInt(2)]
+  const description = reactDescriptions[genRandomInt(2)];
 
   return (
     <header>
@@ -23,14 +23,14 @@ function Header() {
   );
 }
 
-function CoreConcept() {
+function CoreConcept({ image, title, description }) {
   return (
     <li>
-      <img src="..." alt='...'/>
-      <h3>TITLE</h3>
-      <p>DESCRIPTION</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
-  )
+  );
 }
 
 function App() {
@@ -39,16 +39,19 @@ function App() {
       <Header />
       <main>
         <section id="core-comcept">
-        <h2>Core Concept</h2>
-        <ul>
-          <CoreConcept title="Components" description="The core UI building block"
-          image={componentsImg}
-          />
-        </ul>
+          <h2>Core Concept</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+          </ul>
         </section>
       </main>
     </div>
   );
 }
-// 
+//
 export default App;
