5b588d11e4aea8fe281ef23f5b0a52ea952b5eba
alicelee1231 <saemmilee1231@gmail.com>
2024-08-26 22:42:30 +0900

    state studying

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,12 +1,31 @@
-import componentsImg from "./assets/components.png";
+import { useState } from "react";
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/Coreconcept";
 import TabButton from "./components/TabButton";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  function handelSelect() {
-    console.log("hello world - select");
+  //useState는 가장 위에 선언된어야함
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handelSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -29,12 +48,14 @@ function App() {
           <menu>
             {/* <TabButton>Components</TabButton> */}
             {/* <TabButton label="Components" /> */}
-            <TabButton onSelect={handelSelect}>Components</TabButton>
-            <TabButton onSelect={handelSelect}>JSX</TabButton>
-            <TabButton onSelect={handelSelect}>Props</TabButton>
-            <TabButton onSelect={handelSelect}>State</TabButton>
+            <TabButton onSelect={() => handelSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handelSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handelSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handelSelect("state")}>State</TabButton>
           </menu>
-          Dynamic content
+          {tabContent}
         </section>
       </main>
     </div>
