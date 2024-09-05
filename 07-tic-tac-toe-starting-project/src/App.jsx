e43133dc4a82f073df6649ee6794d9f05292f8dc
alicelee1231 <saemmilee1231@gmail.com>
2024-09-05 23:36:04 +0900

    sharing props

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,12 +1,31 @@
+import { act, useState } from "react";
+
 import Player from "./components/Players";
+import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((currActivePlayer) =>
+      currActivePlayer === "X" ? "O" : "X"
+    );
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
 
           {/* <li>
             <span className="player">
@@ -15,7 +34,10 @@ function App() {
             </span>
           </li> */}
         </ol>
-        GAME BOARD
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlyerSymbol={activePlayer}
+        />
       </div>
     </main>
   );
---
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard({ onSelectSquare, activePlyerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      // spread로 해야 기존의 데이터를 바꾸지 않고 진행할 수 있음
+      // 만약 바로 prevGameboard를 변경해버리면 다른 곳에서 사용하는 값들도 같이 변경돼서
+      // side affect가 올 수 있음
+      const updateBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updateBoard[rowIndex][colIndex] = activePlyerSymbol;
+      return updateBoard;
+    });
+
+    onSelectSquare();
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                {/* 인수가 필요할 때는 익명함수를 사용하기 */}
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
