df2dab87da4c03a4a5a379a4ec0db66d4acd5f64
alicelee1231 <saemmilee1231@gmail.com>
2024-09-18 15:36:57 +0900

    tailwind studying

--- 01-starting-project_st/src/components/AuthInputs.jsx
+++ 01-starting-project_st/src/components/AuthInputs.jsx
@@ -1,14 +1,6 @@
 import { useState } from "react";
-import { styled } from "styled-components";
 import Button from "./Buttons";
 import CustomInput from "./Input";
-
-const ControlDivContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -31,26 +23,29 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlDivContainer>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+    >
+      <div className="flex flex-col gap-2 mb-6">
         <CustomInput
           label="Email"
-          invaild={emailNotValid}
+          invalid={emailNotValid}
           type="email"
           // style={{ backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db" }}
           onChange={(event) => handleInputChange("email", event.target.value)}
         />
         <CustomInput
-          invaild={passwordNotValid}
+          invalid={passwordNotValid}
           label="Password"
           type="password"
           onChange={(event) =>
             handleInputChange("password", event.target.value)
           }
         />
-      </ControlDivContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
