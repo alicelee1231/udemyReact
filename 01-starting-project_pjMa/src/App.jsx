7198b286b8eb7f0baaf4f6daa6e35d682966bd21
alicelee1231 <saemmilee1231@gmail.com>
2024-09-25 17:56:26 +0900

    practice project

--- 01-starting-project_pjMa/src/App.jsx
+++ 01-starting-project_pjMa/src/App.jsx
@@ -1,8 +1,51 @@
+import NewProject from "./components/NewProject";
+import NoPrjSelect from "./components/NoPrjSelect";
+import ProjectSibebar from "./components/ProjectSidebar";
+import { useState } from "react";
+
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedPjId: undefined,
+    projects: [],
+  });
+
+  function hanldeStartAppProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedPjId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectState);
+
+  let content;
+
+  if (projectState.selectedPjId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectState.selectedPjId === undefined) {
+    content = <NoPrjSelect onStartAddProject={hanldeStartAppProject} />;
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSibebar onStartAddProject={hanldeStartAppProject} />
+      {content}
+    </main>
   );
 }
 
