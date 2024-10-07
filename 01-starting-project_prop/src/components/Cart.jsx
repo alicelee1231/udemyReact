46b9a6394837d1cecbc3c2a457842c8a51ee8d88
alicelee1231 <saemmilee1231@gmail.com>
2024-10-07 17:33:01 +0900

    context studying

--- 01-starting-project_prop/src/components/Cart.jsx
+++ 01-starting-project_prop/src/components/Cart.jsx
@@ -1,9 +1,8 @@
 import { useContext } from "react";
 import { CartContext } from "../store/shopping-cart-context";
 
-export default function Cart({ onUpdateItemQuantity }) {
-  const cartCtx = useContext(CartContext);
-  const items = cartCtx?.items || []; // items가 undefined일 경우 빈 배열 사용
+export default function Cart() {
+  const { items, updateItemQuantity } = useContext(CartContext);
 
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
@@ -26,11 +25,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
